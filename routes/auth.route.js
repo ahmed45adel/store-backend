83da9309fd7dccd76739230786e1d2b0bdde829f
Ahmed Adel <ahmed45adel@yahoo.com>
2025-07-20 16:57:00 +0300

    handle typo

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/refreshToken", refreshToken);
+router.post("/refresh-token", refreshToken);
 router.get("/profile", protectRoute, getProfile);
 
 export default router;
