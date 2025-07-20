39cc0a53325ebbf06024716cde9be147f7559d5a
Ahmed Adel <ahmed45adel@yahoo.com>
2025-07-20 17:14:26 +0300

    handle cross site

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -22,13 +22,15 @@ const setCookies = (res, accessToken, refreshToken) => {
 	res.cookie("accessToken", accessToken, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === "production",
-		sameSite: "strict",
+		secure: true,
+  		sameSite: "none",
 		maxAge: 15 * 60 * 1000, // 15 minutes
 	});
 	res.cookie("refreshToken", refreshToken, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === "production",
-		sameSite: "strict",
+		secure: true,
+  		sameSite: "none",
 		maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 	});
 };
